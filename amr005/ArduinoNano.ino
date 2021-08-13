8f2daf4920f0b620931a5fd2e76b775b8373df24
refracta <58779799+refracta@users.noreply.github.com>
2021-08-14 00:43:38 +0900

    Add toggle button

--- amr005/ArduinoNano.ino
+++ amr005/ArduinoNano.ino
@@ -2,17 +2,18 @@
 #define DEBUG_MODE false
 
 #if DEBUG_MODE
-  #define debug(log) Serial.print(log);
-  #define debugMemory() Serial.print(F("Left Memory: ")); Serial.println(freeMemory());
+#define debug(log) Serial.print(log);
+#define debugMemory() Serial.print(F("Left Memory: ")); Serial.println(freeMemory());
 #else
-  #define debug(log)
-  #define debugMemory()
+#define debug(log)
+#define debugMemory()
 #endif
 
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
 
 #define RELAY_PIN 2
+#define BUTTON_PIN 12
 
 const int SERIAL_BAUD_RATE = 9600;
 const int NODEMCU1_BAUD_RATE = 9600;
@@ -25,38 +26,55 @@ void setupSerial() {
   NodeMCU1.begin(NODEMCU1_BAUD_RATE);
 }
 
-void setupRelay(){
-  digitalWrite(RELAY_PIN, HIGH);
-  pinMode(RELAY_PIN, OUTPUT);
-}
+// const unsigned int NUMBER_OF_MODES = 2;
+__attribute__((section(".noinit"))) unsigned int mode;
 
-void setup() {
-  setupSerial();
-  setupRelay();
+void setupMode(){
+    // if (++mode >= NUMBER_OF_MODES) mode = 0;
 }
 
 boolean LIGHT_STATUS = true;
 
+void setupRelay() {
+  pinMode(RELAY_PIN, OUTPUT);
+  if(mode == 0){
+    digitalWrite(RELAY_PIN, HIGH);
+    LIGHT_STATUS = true;
+  } else {
+    digitalWrite(RELAY_PIN, LOW);
+    LIGHT_STATUS = false;
+  }
+  mode = LIGHT_STATUS ? 1 : 0;
+}
+
+void setup() {
+  setupMode();
+  setupSerial();
+  setupRelay();
+}
+
 unsigned long LAST_UPDATE_STATUS_TIME = 0;
 unsigned long UPDATE_STATUS_DELAY = 5 * 1000;
+
 void handleNodeMCUSerial(DynamicJsonDocument& doc) {
   debugMemory();
   NodeMCU1.println(F("{\"type\":\"UpdateStatus\",\"isBusy\":true}"));
   String type = doc[F("type")];
-  if(String(F("LightOn")).equals(String(type))){
-  digitalWrite(RELAY_PIN, HIGH);
-  LIGHT_STATUS = true;
-  }else if(String(F("LightOff")).equals(String(type))){
+  if (String(F("LightOn")).equals(String(type))) {
+    digitalWrite(RELAY_PIN, HIGH);
+    LIGHT_STATUS = true;
+  } else if (String(F("LightOff")).equals(String(type))) {
     digitalWrite(RELAY_PIN, LOW);
-  LIGHT_STATUS = false;
-  }else if(String(F("LightToggle")).equals(String(type))){
-  if(LIGHT_STATUS) {
-    digitalWrite(RELAY_PIN, LOW);
-  } else {
-    digitalWrite(RELAY_PIN, HIGH);  
+    LIGHT_STATUS = false;
+  } else if (String(F("LightToggle")).equals(String(type))) {
+    if (LIGHT_STATUS) {
+      digitalWrite(RELAY_PIN, LOW);
+    } else {
+      digitalWrite(RELAY_PIN, HIGH);
+    }
+    LIGHT_STATUS = !LIGHT_STATUS;
   }
-  LIGHT_STATUS = !LIGHT_STATUS;
-  }
+  mode = LIGHT_STATUS ? 1 : 0;
   NodeMCU1.println(F("{\"type\":\"UpdateStatus\",\"isBusy\":false}"));
   LAST_UPDATE_STATUS_TIME = millis();
   debugMemory();
@@ -76,7 +94,7 @@ void runNodeMCUSerial() {
       handleNodeMCUSerial(doc);
     }
   } else {
-    if(millis() - LAST_UPDATE_STATUS_TIME > UPDATE_STATUS_DELAY){
+    if (millis() - LAST_UPDATE_STATUS_TIME > UPDATE_STATUS_DELAY) {
       NodeMCU1.println(F("{\"type\":\"UpdateStatus\",\"isBusy\":false}"));
       debug(F("UpdateStatus to NodeMCU1\n"));
       LAST_UPDATE_STATUS_TIME = millis();
